c0bf32cdc0f54d6fab7f3068840ae6462145de46
Martin Van Aken <martin@joyouscoding.com>
2021-10-25 15:12:11 +0200

    details with props

--- App.js
+++ App.js
@@ -1,23 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 const Stack = createNativeStackNavigator();
 
-function Home() {
+function Home(props) {
+  let navigation = props.navigation;
   return (
     <View>
       <Text>Home</Text>
+      <Button
+        title="Go to Detail Bob"
+        onPress={() => navigation.navigate('Details', { name: "Bob" })}
+        />
+      <Button
+        title="Go to Detail Rachida"
+        onPress={() => navigation.navigate('Details', { name: "Rachida" })}/>
     </View>
   );
 }
 
-function Detail() {
+function Detail(props) {
+  let route = props.route;
+  let name = route.params.name;
   return (
     <View>
-      <Text>Detail</Text>
+      <Text>Detail for {name}</Text>
     </View>
   );
 }
